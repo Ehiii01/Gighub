e17f7ea448cc0feb27253443a37ddb69b927fce0
Iriajen obehi <obehiiriajen@gmail.com>
2025-12-09 16:14:30 +0100

    Added 4 changes (Added a migration to poplulate Genres table)

---
+++ GigHub/GigHub/Migrations/20251208180616_PopulateGenresTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GigHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateGenresTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                    table: "Genres",
+                    columns: new[] { "Id", "Name" },
+                    values: new object[,]
+                    {
+                        { "1", "Jazz" },
+                        { "2", "Blues" },
+                        { "3", "Rock" },
+                        { "4", "Country" },
+                    });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(table: "Genres", keyColumn: "Id", keyValue: 1);
+
+
+            migrationBuilder.DeleteData(table: "Genres", keyColumn: "Id", keyValue: 2);
+
+            migrationBuilder.DeleteData(table: "Genres", keyColumn: "Id", keyValue: 3);
+
+
+            migrationBuilder.DeleteData(table: "Genres", keyColumn: "Id", keyValue: 4);
+        }
+    }
+}
